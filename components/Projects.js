2b07d673b5223d871fc86943ac1b1cc4d5749e14
Ochaart <ochaart@users.noreply.github.com>
2021-06-04 16:24:26 -0700

    updated css and animations for projects

--- components/Projects.js
+++ components/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import ParticleBackground from './ParticleBackground';
 import { motion, useAnimation } from 'framer-motion';
 import { InView } from 'react-intersection-observer';
 import styles from '../styles/Projects.module.css'
@@ -11,16 +12,16 @@ const projectVariants = {
     opacity: 0
   },
   catWalkEnd: {
-    x: -500,
-    y: -100,
+    x: "-26vw",
+    y: "-8vh",
     opacity: 1,
     transition: {
       duration: .5
     }
   },
   stitchEnd: {
-    x: 40,
-    y: 200,
+    x: "3vw",
+    y: "25vh",
     opacity: 1,
     transition: {
       duration: .5,
@@ -28,12 +29,30 @@ const projectVariants = {
     }
   },
   lazyChef: {
-    x: 550,
-    y: -200,
+    x: "28vw",
+    y: "-21vh",
     opacity: 1,
     transition: {
       duration: .5,
       delay: .3,
+    }
+  },
+  titleStart: {
+    x: -10,
+    opacity: 0,
+  },
+  titleEnd: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: .4
+    }
+  },
+  hover: {
+    border: "8px solid #800000",
+    opacity: .5,
+    transition: {
+      duration: .3,
     }
   }
 }
@@ -44,20 +63,30 @@ const Projects = () => {
     <InView threshold={.60} triggerOnce={true}>
       {({ inView, ref }) => (
         <section ref={ref} className={styles.projects}>
+          <ParticleBackground />
+          <motion.div className={styles.title}
+            variants={projectVariants}
+            initial="titleStart"
+            animate={inView ? "titleEnd" : "titleStart"}
+          >
+            P R O J E C T S
+          </motion.div>
           <motion.div className={styles.container}
             variants={projectVariants}
             initial="start"
-            animate={inView? "catWalkEnd" : "start"}
+            whileHover="hover"
+            animate={inView ? "catWalkEnd" : "start"}
           >
             <Image
               src="/catwalk.jpeg"
               layout="fill"
             />
           </motion.div>
-          <motion.div className={styles.container}
+          <motion.div className={styles.container1}
             variants={projectVariants}
             initial="start"
-            animate={inView? "stitchEnd" : "start"}
+            whileHover="hover"
+            animate={inView ? "stitchEnd" : "start"}
           >
             <Image
               src="/stitchSaver.jpeg"
@@ -67,10 +96,11 @@ const Projects = () => {
           <motion.div className={styles.container2}
             variants={projectVariants}
             initial="start"
-            animate={inView? "lazyChef" : "start"}
+            whileHover="hover"
+            animate={inView ? "lazyChef" : "start"}
           >
             <Image
-              src="/lazyChef.png"
+              src="/lazyChef.jpeg"
               layout="fill"
             />
           </motion.div>
